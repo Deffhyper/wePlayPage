1741f0d47fb52d1df91e05b4818ca6e909b4d737
Deff <olegkotsubko@gmail.com>
2018-07-16 01:25:46 +0300

    add responsive

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,4 +1,6 @@
 import './../styles/main.scss'
+import $ from 'jquery'
+import { Power2, TimelineMax } from 'gsap/TweenMax'
 
 if (process.env.NODE_ENV !== 'production') {
   require('./../index.pug')
@@ -9,72 +11,31 @@ export class Common {
     this.init()
   }
 
-  dropdown () {
-    const dopdownTrigger = document.querySelectorAll('.dropdown__trigger')
-    for (let trigger of dopdownTrigger) {
-      trigger.addEventListener('click', function () {
-        this.nextSibling.classList.toggle('show')
-      })
-    }
+  socialLinkAnimation () {
+    let tl = new TimelineMax()
+    tl.staggerFromTo($('.socials-list__item'), 0.6, {
+      y: -15,
+      opacity: 0
+    }, {
+      y: 0,
+      opacity: 1,
+      ease: Power2.easeOut
+    }, 0.3)
   }
-
-  closeDropdown () {
-    window.onclick = function (event) {
-      if (!event.target.matches('.dropdown__trigger')) {
-        const dropdowns = document.querySelectorAll('.dropdown__content')
-        for (let dropdown of dropdowns) {
-          let openDropdown = dropdown
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show')
-          }
-        }
-      }
-    }
-  }
-
-  openMobMenu () {
-    const mobTrigger = document.getElementById('mob-menu-trigger')
-    let overlay = document.createElement('div')
-    overlay.className = 'blur'
-
-    mobTrigger.addEventListener('click', function () {
-      this.classList.toggle('active')
-      document.querySelector('.header').classList.toggle('menu-open')
-
-      if (this.classList.contains('active')) {
-        document.body.appendChild(overlay)
-        document.body.classList.add('top-menu-open')
-      } else {
-        overlay.remove()
-        document.body.classList.remove('top-menu-open')
-      }
-      overlay.addEventListener('click', function () {
-        document.querySelector('.header').classList.remove('menu-open')
-        mobTrigger.classList.remove('active')
-        document.body.classList.remove('top-menu-open')
-        this.remove()
-        this.removeEventListener('click')
-      })
+  decorAnimation () {
+    let tl = new TimelineMax()
+    tl.fromTo($('.decor'), 0.6, {
+      x: 15,
+      opacity: 0
+    }, {
+      x: 0,
+      opacity: 1,
+      ease: Power2.easeOut
     })
   }
-
-  modalOpen () {
-    const modalArray = document.querySelectorAll('.modal-trigger')
-    for (let modal of modalArray) {
-      modal.addEventListener('click', function (event) {
-        event.preventDefault()
-        let modalDataAttr = this.dataset.modal
-
-        document.querySelector(modalDataAttr).style.display = 'block'
-      })
-    }
-  }
-
   init () {
-    this.dropdown()
-    this.closeDropdown()
-    this.openMobMenu()
-    this.modalOpen()
+    this.socialLinkAnimation()
+    this.decorAnimation()
   }
 }
 export default new Common()
